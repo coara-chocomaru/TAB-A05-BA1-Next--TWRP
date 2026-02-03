801f295db1d111047e5fc9aad21a339d2f0c7736
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-02-04 01:07:19 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,13 @@ TARGET_NO_FACTORYIMAGE:=true
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x40078000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += "bootopt=64S3,32N2,64N2"
+BOARD_KERNEL_CMDLINE += "androidboot.selinux=permissive"
+BOARD_KERNEL_CMDLINE += "androidboot.vbmeta.avb_version=1.1"
+BOARD_KERNEL_CMDLINE += "androidboot.vbmeta.device_state=unlocked"
+BOARD_KERNEL_CMDLINE += "androidboot.vbmeta.invalidate_on_error=no"
+BOARD_KERNEL_CMDLINE += "androidboot.veritymode=ignore_corruption"
+BOARD_KERNEL_CMDLINE += "androidboot.dtbo_idx=0"
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x14f88000
 BOARD_KERNEL_TAGS_OFFSET := 0x13f88000
@@ -174,4 +180,3 @@ RECOVERY_TOUCHSCREEN_SWAP_XY := true
 RECOVERY_TOUCHSCREEN_FLIP_Y := true
 
 #include vendor/sts/a05ba/BoardConfigVendor.mk
-
