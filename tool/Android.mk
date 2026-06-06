61735abb09b85f11ae3b539c06018565f156943d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-07 08:21:41 +0900

    Add Android.mk for coara executable build

---
+++ tool/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE        := coara
+LOCAL_MODULE_TAGS   := optional
+LOCAL_SRC_FILES     := coara.cpp
+
+LOCAL_CPPFLAGS      := -std=c++11 -Wall -Wextra
+
+LOCAL_STATIC_LIBRARIES :=
+LOCAL_SHARED_LIBRARIES := liblog
+
+LOCAL_FORCE_STATIC_EXECUTABLE := false
+LOCAL_MODULE_CLASS  := EXECUTABLES
+LOCAL_MODULE_PATH   := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+
+include $(BUILD_EXECUTABLE)
